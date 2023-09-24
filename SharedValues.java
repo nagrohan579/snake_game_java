665185fead6451cfd3e521c00b515f7ac8156950
Rohan Nag <nagrohan579@gmail.com>
2023-09-24 22:18:58 +0530

    Added SharedValues.java file, which contains all the values shared throughout the application

---
+++ SharedValues.java
@@ -0,0 +1,9 @@
+public class SharedValues {
+    static final int SCREEN_WIDTH = 700;
+    static final int SCREEN_HEIGHT = 700;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    static int bodyParts = 6;
+    static int applesEaten = 0;
+}
