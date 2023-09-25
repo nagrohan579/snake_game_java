8624fe27faf65c1883d4522e947317785044b703
Rohan Nag <nagrohan579@gmail.com>
2023-09-25 08:07:36 +0530

    Changed the colors of the Score board

--- ScorePanel.java
+++ ScorePanel.java
@@ -7,13 +7,13 @@ public class ScorePanel extends JPanel {
     public ScorePanel()
     {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,2*UNIT_SIZE));
-        this.setBackground(new Color(0xffe3f1));
+        this.setBackground(new Color(0x9E6F99));
         this.setLayout(new BorderLayout());
 
         score = new JLabel("SCORE : " + applesEaten);
         score.setHorizontalAlignment(JLabel.CENTER);
         score.setFont(new Font("Rockwell",Font.BOLD,40));
-        score.setForeground(new Color(0x00c9c8));
+        score.setForeground(new Color(0xffe3f1));
 
         this.add(score,BorderLayout.CENTER);
     }
